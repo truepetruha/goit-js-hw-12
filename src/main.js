1da01bc4f200651336f4eac2bb31d25265aa3b10
truepetruha <142122334+truepetruha@users.noreply.github.com>
2024-04-07 21:15:09 +0300

    finish

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import axios from 'axios';
+
 const modalLightboxGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
@@ -11,73 +13,148 @@ const modalLightboxGallery = new SimpleLightbox('.gallery a', {
 
 const searchForm = document.querySelector('.js-search-form');
 const ulEl = document.querySelector('.list-photo');
+const btnLoadMore = document.querySelector('[data-action="load-more"]');
 const loader = document.querySelector('.loader');
+const loaderLoadMore = document.querySelector('.loader-load-more');
+
+const hiddenClass = 'is-hidden';
+
+let page = 1;
+
+let maxPage = 0;
+
+let query = '';
+
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '41849458-2d98265cf06659a45ba73a30c';
 
 searchForm.addEventListener('submit', handleSearch);
-loader.style.display = 'none';
 
-function handleSearch(event) {
+async function handleSearch(event) {
   event.preventDefault();
 
-  loader.style.display = 'inline-block';
-
   ulEl.innerHTML = '';
-  const form = event.currentTarget;
-  const query = form.elements.query.value.trim();
 
-  if (query === '') {
+  loader.classList.remove(hiddenClass);
+
+  page = 1;
+
+  const form = event.currentTarget;
+  query = form.elements.query.value.trim();
+
+  if (!query) {
+    loader.classList.add(hiddenClass);
+    btnLoadMore.classList.add(hiddenClass);
+
     iziToast.show({
       title: 'Error',
+      titleSize: '30',
+      messageSize: '25',
       color: 'yellow',
       message: 'Please search for something',
     });
-    loader.style.display = 'none';
     return;
   }
 
-  searchPhoto(query)
-    .then(data => {
-      if (!data.hits.length) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
+  try {
+    const { hits, totalHits } = await getPhotos(query);
 
-        return;
-      }
-      ulEl.innerHTML = markupPhoto(data.hits);
-      modalLightboxGallery.refresh();
-    })
-    .catch(onFetchError)
-    .finally(() => {
-      form.reset();
-      loader.style.display = 'none';
-    });
-  // проблема була в тому що я вставив loader.style.display = 'none' не у файналі, тобто промахнувся і вставив його за дужку, воно було під ним і мабуть тому воно не показувало)
-}
-function searchPhoto(value) {
-  const BASE_URL = 'https://pixabay.com/api';
-  const API_KEY = '41849458-2d98265cf06659a45ba73a30c';
-  const urlParams = new URLSearchParams({
-    key: API_KEY,
-    q: value,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  const url = `${BASE_URL}/?${urlParams}`;
-  return fetch(url).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
+    maxPage = Math.ceil(totalHits / 40);
+
+    markupPhoto(hits, ulEl);
+    if (hits.length > 0 && hits.length !== totalHits) {
+      btnLoadMore.classList.remove(hiddenClass);
+      btnLoadMore.addEventListener('click', handleLoadMore);
+    } else if (!hits.length) {
+      btnLoadMore.classList.add(hiddenClass);
+
+      iziToast.error({
+        title: 'Error',
+        titleSize: '30',
+        messageSize: '25',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      btnLoadMore.classList.add(hiddenClass);
     }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.classList.add(hiddenClass);
 
-    return resp.json();
-  });
+    form.reset();
+  }
 }
 
-function markupPhoto(arr) {
-  return arr
+async function getPhotos(value, page = 1) {
+  try {
+    const response = await axios.get(`${BASE_URL}/`, {
+      params: {
+        key: API_KEY,
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 40,
+        page,
+      },
+    });
+    return response.data;
+  } catch {
+    iziToast.error({
+      title: 'Error',
+      titleSize: '30',
+      messageSize: '25',
+      message: 'Sorry! Try later! Server not working',
+    });
+    console.error(error.message);
+  }
+}
+
+// ------------------------------------------------------------КНОПКА LOAD MORE-------------------------------------------------------------
+
+async function handleLoadMore() {
+  page += 1;
+  loaderLoadMore.classList.remove(hiddenClass);
+  btnLoadMore.classList.add(hiddenClass);
+
+  const getHeightImgCard = document
+    .querySelector('.gallery-item')
+    .getBoundingClientRect();
+
+  try {
+    const { hits } = await getPhotos(query, page);
+    markupPhoto(hits, ulEl);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    window.scrollBy({
+      top: getHeightImgCard.height * 2,
+      left: 0,
+      behavior: 'smooth',
+    });
+    loaderLoadMore.classList.add(hiddenClass);
+    btnLoadMore.classList.remove(hiddenClass);
+    if (page === maxPage) {
+      btnLoadMore.classList.add(hiddenClass);
+      btnLoadMore.removeEventListener('click', handleLoadMore);
+      iziToast.show({
+        title: 'Hey',
+        titleSize: '30',
+        messageSize: '25',
+        color: 'blue',
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+    }
+  }
+}
+
+// ------------------------------------------------------------РОЗМІТКА--------------------------------------------------------------------
+
+function markupPhoto(hits) {
+  const markup = hits
     .map(
       ({
         webformatURL,
@@ -108,13 +185,6 @@ function markupPhoto(arr) {
         </li>`
     )
     .join('');
-}
-
-function onFetchError(error) {
-  console.error(error);
-  iziToast.error({
-    title: 'Error',
-    message:
-      'Sorry, there are no images matching your search query. Please try again!',
-  });
+  ulEl.insertAdjacentHTML('beforeend', markup);
+  modalLightboxGallery.refresh();
 }
